ceaf836b027a76bb619283c39ac203d506ed0dbb
Robert Dionne <robertsdionne@gmail.com>
2013-11-24 17:29:26 -0500

    Make variables local.

--- embeddings.lua
+++ embeddings.lua
@@ -4,10 +4,10 @@ local EMBEDDING_COUNT = 130000
 local EMBEDDING_DIMENSION = 50
 
 function loadEmbeddings(embeddings_filename)
-  embeddings_file = torch.DiskFile(embeddings_filename)
-  raw_embeddings = torch.Tensor(
+  local embeddings_file = torch.DiskFile(embeddings_filename)
+  local raw_embeddings = torch.Tensor(
     embeddings_file:readDouble(EMBEDDING_DIMENSION * EMBEDDING_COUNT))
-  embeddings = torch.Tensor(EMBEDDING_COUNT, EMBEDDING_DIMENSION)
+  local embeddings = torch.Tensor(EMBEDDING_COUNT, EMBEDDING_DIMENSION)
   embeddings:copy(raw_embeddings)
   return embeddings:t()
 end
