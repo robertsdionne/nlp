704ecb6d49d2bffbddeb37eb32d19fc58f2a0a24
Robert Scott Dionne <robertsdionne@gmail.com>
2013-11-28 17:14:54 -0500

    Pull the vocabulary and tag sets from the training data.

--- modules/DataLoader.lua
+++ modules/DataLoader.lua
@@ -7,11 +7,22 @@ TEST_FILENAME = '../data/en-wsj-test.tagged'
 -- The training data.
 TRAIN_FILENAME = '../data/en-wsj-train.pos'
 
+local function convertSetToList(set)
+  local list = {}
+  local i = 1
+  for item, _ in pairs(set) do
+    list[i] = item
+    i = i + 1
+  end
+  table.sort(list)
+  return list
+end
+
 -- Function to load a data file into a table of nn.TaggedSentences.
 function loadData(data_filename)
   -- Open the data file.
   local data_file = torch.DiskFile(data_filename)
-  local tagged_sentences, words, tags = {}, {}, {}
+  local tagged_sentences, sentence_words, sentence_tags, word_set, tag_set = {}, {}, {}, {}, {}
   local i, j = 1, 1
   repeat
     -- Use pcall to safely invoke data_file:readString('*l') since readString throws an error upon
@@ -23,15 +34,18 @@ function loadData(data_filename)
     if '' ~= line then
       -- If the line is not empty, read its (word, tag) pair.
       for word, tag in line:gmatch('(.*)%s+(.*)') do
-        words[j] = word
-        tags[j] = tag
+        sentence_words[j] = word
+        sentence_tags[j] = tag
+        word_set[word] = true
+        tag_set[tag] = true
         j = j + 1
       end
     else
-      -- If the line is empty, assemble the words and tags into a nn.TaggedSentence.
-      tagged_sentences[i] = nn.TaggedSentence(words, tags)
-      words = {}
-      tags = {}
+      -- If the line is empty, assemble the sentence_words and sentence_tags
+      -- into a nn.TaggedSentence.
+      tagged_sentences[i] = nn.TaggedSentence(sentence_words, sentence_tags)
+      sentence_words = {}
+      sentence_tags = {}
       j = 1
       i = i + 1
     end
@@ -41,5 +55,5 @@ function loadData(data_filename)
   data_file:close()
 
   -- Return all the nn.TaggedSentences.
-  return tagged_sentences
+  return tagged_sentences, convertSetToList(word_set), convertSetToList(tag_set)
 end
--- modules/DataLoaderTest.lua
+++ modules/DataLoaderTest.lua
@@ -1,11 +1,12 @@
 require 'torch'
 require 'nn'
 
+dofile 'LoadedLookupTable.lua'
 dofile 'TaggedSentence.lua'
 dofile 'DataLoader.lua'
 
 -- Test loadData() to ensure it reads the entire data file.
-tagged_sentences = loadData(TRAIN_FILENAME)
+local tagged_sentences, vocabulary, tags = loadData(TRAIN_FILENAME)
 
 -- The training data has 39815 examples.
 assert(39815 == #tagged_sentences)
@@ -22,3 +23,77 @@ assert("In_IN an_DT Oct._NNP 19_CD review_NN of_IN ``_`` The_DT Misanthrope_NN '
 assert("That_DT could_MD cost_VB him_PRP the_DT chance_NN to_TO influence_VB the_DT outcome_NN " ..
   "and_CC perhaps_RB join_VB the_DT winning_VBG bidder_NN ._." ==
       tagged_sentences[39815]:toString())
+
+assert(40366 == #vocabulary)
+assert(48 == #tags)
+
+-- local function tokenizeNumber(number)
+--   local state = 0
+--   local size = 0
+--   local idx = 1
+--   local finished = false
+
+--   while not finished do
+--     local c = number:sub(idx, idx)
+--     idx = idx + 1
+
+--     if '' == c then
+--       break
+--     end
+
+--     if 0 == state then
+--       if c == '+' or c == '-' then
+--         state = 1
+--       elseif c == '.' or c == ',' then
+--         state = 2
+--       elseif c:match('%d') then
+--         state = 4
+--       else
+--         finished = true
+--       end
+--     elseif 1 == state then
+--       if c == '.' or c == ',' then
+--         state = 2
+--       elseif c:match('%d') then
+--         state = 4
+--       else
+--         finished = true
+--       end
+--     elseif 2 == state then
+--       if c:match('%d') then
+--         state = 4
+--       else
+--         finished = true
+--       end
+--     elseif 3 == state then
+--       if c:match('%d') then
+--         state = 4
+--       else
+--         finished = true
+--       end
+--     elseif 4 == state then
+--       size = idx - 1
+--       if c == '.' or c == ',' then
+--         state = 3
+--       elseif c:match('%d') then
+--         state = 4
+--       else
+--         finished = true
+--       end
+--     end
+--   end
+
+--   return size
+-- end
+
+local table = nn.LoadedLookupTable.load()
+
+for _, word in pairs(vocabulary) do
+ word = word:lower()
+ word = word:gsub('[0-9]+', '0')
+ local index = table:queryIndex(word)
+ if nil == index then
+   index = 'UNKNOWN'
+ end
+ print(word .. ': ' .. index)
+end
