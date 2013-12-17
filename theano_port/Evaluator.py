948e761c767c8fe5980ba07f3088b8a320561044
Robert Scott Dionne <robertsdionne@gmail.com>
2013-12-16 20:37:41 -0500

    Implement the evaluator.

--- theano_port/Evaluator.py
+++ theano_port/Evaluator.py
@@ -7,8 +7,24 @@ class Evaluator(object):
     num_unknown_words = 1.0
     num_unknown_words_correct = 1.0
     num_decoding_inversions = 0.0
+    i = 0
     for tagged_sentence in tagged_sentences:
+      if i % 100 == 0:
+        print('finished ' + i + ' sentences / ' + len(tagged_sentences))
+      i += 1
       words = tagged_sentence.words
       gold_tags = tagged_sentence.tags
       guessed_tags = pos_tagger.tag(tagged_sentence)
-      
+      for position in xrange(0, len(words)):
+        word = words[position]
+        gold_tag = gold_tags[position]
+        guessed_tag = guessed_tags[position]
+        if guessed_tag == gold_tag:
+          num_tags_correct += 1.0
+        num_tags += 1.0
+        if word not in training_vocabulary:
+          if guessed_tag == gold_tag:
+            num_unknown_words_correct += 1.0
+          num_unknown_words += 1.0
+    print('  Tag Accuracy: ' + (num_tags_correct / num_tags))
+    print('  (Unknown Accuracy: ' + (num_unknown_words_correct / num_unknown_words) + ')')
